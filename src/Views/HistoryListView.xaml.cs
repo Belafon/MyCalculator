71a9b7d3843905b62cb585e5c096d472aa19da1c
Ondrej Tichavsky <tichavskyo@seznam.cz>
2024-10-07 02:02:38 +0200

    assembly info removed

--- src/Views/HistoryListView.xaml.cs
+++ src/Views/HistoryListView.xaml.cs
@@ -4,24 +4,24 @@ using MyCalculator.ViewModels;
 namespace MyCalculator.Views;
 public partial class HistoryListView : UserControl
 {
-    public HistoryListView()
-    {
-        InitializeComponent();
+	public HistoryListView()
+	{
+		InitializeComponent();
 
-        DataContextChanged += (s, e) =>
-        {
-            var viewModel = DataContext as HistoryListViewModel;
-            if (viewModel != null)
-            {
-                viewModel.ScrollToBottom = () =>
-                {
-                    if (HistoryListBox.Items.Count > 0)
-                    {
-                        var lastItem = HistoryListBox.Items[^1];
-                        HistoryListBox.ScrollIntoView(lastItem);
-                    }
-                };
-            }
-        };
-    }
+		DataContextChanged += (s, e) =>
+		{
+			var viewModel = DataContext as HistoryListViewModel;
+			if (viewModel != null)
+			{
+				viewModel.ScrollToBottom = () =>
+				{
+					if (HistoryListBox.Items.Count > 0)
+					{
+						var lastItem = HistoryListBox.Items[^1];
+						HistoryListBox.ScrollIntoView(lastItem);
+					}
+				};
+			}
+		};
+	}
 }
